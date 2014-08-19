82e47085cc01ecd55a121fc1f06a7615259f238a
Luis Naia <luis.naia@heanet.ie>
2014-08-19 16:15:57 +0100

    Interface prototype *IN PROGRESS*

--- src/js/app.angular.js
+++ src/js/app.angular.js
@@ -14,7 +14,7 @@
       };
     })
 
-    .factory('persistenceService', function () {
+    .factory('persistenceService', function (generateGuid) {
       var persistence = {};
 
       persistence.set = function (items, callback) {
@@ -29,21 +29,63 @@
         });
       };
 
+      persistence.findAndReplace = function (items, item) {
+        var l = items.length, i;
+
+        for (i = 0; i < l; i++) {
+          if (items[i].guid === item.guid) {
+            items[i] = item;
+            return true;
+          }
+        }
+        return false;
+      };
+
+      persistence.save = function (item, successCallback) {
+        persistence.get({items: []}, function (results) {
+
+          item.enabled = true;
+
+          if (!item.guid) {
+            item.guid = generateGuid();
+            results.items.push(item);
+          } else {
+            persistence.findAndReplace(results.items, item);
+          }
+
+          persistence.set(results, function () {
+            console.debug('saved successfully');
+            successCallback ? successCallback() : null;
+          });
+        });
+      };
+
       return persistence;
     })
 
+    .factory('manifest', function ($http) {
+      return (function (callback) {
+        $http({method: 'GET', url: '/manifest.json'}).success(function (data) {
+          callback(data);
+        });
+      });
+    })
+
     .factory('generateGuid', function () {
-      return (function() {
+      return (function () {
         function s4() {
           return Math.floor((1 + Math.random()) * 0x10000)
             .toString(16)
             .substring(1);
         }
-        return function() {
+
+        return function () {
           return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
             s4() + '-' + s4() + s4() + s4();
         };
       }())
     });
+
+
 }(chrome));
 
--- src/js/options.controller.js
+++ src/js/options.controller.js
@@ -7,52 +7,37 @@
     '$scope',
     'persistenceService',
     'generateGuid',
-    '$http',
-    function ($scope, persistenceService, generateGuid, $http) {
+    'manifest',
+    function ($scope, persistenceService, generateGuid, manifest) {
 
-      $http({method: 'GET', url: '/manifest.json'}).success(function (data) {
+      manifest(function (data) {
         $scope.manifest = data;
       });
 
-
       $scope.save = function () {
-        var item;
-
-        if (!this.inject) {
-          return;
-        }
-
-        item = this.inject;
-        item.enabled = true;
-
-        persistenceService.get({items: []}, function (results) {
-          var l = results.items.length, i;
-
-          if (!item.guid) {
-            item.guid = generateGuid();
-            results.items.push(item);
-          } else {
-            for (i = 0; i < l; i++) {
-              if (results.items[i].guid === item.guid) {
-                results.items[i] = item;
-                break;
-              }
-            }
-          }
-
-          persistenceService.set(results, function () {
-            console.debug('saved successfully');
+        if (this.inject) {
+          persistenceService.save(this.inject, function (){
+            console.log('controller success callback');
           });
-        });
-
+        }
       };
 
-      persistenceService.get({items: []}, function (results) {
-        if (results.items.length > 0) {
-          $scope.inject = results.items[0];
-          $scope.$apply();
-        }
-      });
+      $scope.delete = function (item) {
+        persistenceService.delete(item.guid);
+        listItems();
+      };
+
+       function listItems() {
+        persistenceService.get({items: []}, function (results) {
+          if (results.items.length > 0) {
+            $scope.items = results.items;
+            $scope.$apply();
+          }
+        });
+      }
+
+      listItems();
+
     }
   ])
   ;
